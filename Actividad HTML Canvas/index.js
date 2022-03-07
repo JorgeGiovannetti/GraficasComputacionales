b35a52bc4cbd3f7ca395f523cf07b92aada77e58
JorgeGiovannetti <giovannetti2000@gmail.com>
2022-03-06 23:28:36 -0600

    Kirby done

--- Actividad HTML Canvas/index.js
+++ Actividad HTML Canvas/index.js
@@ -1,9 +1,9 @@
 const drawKirby = () => {
-  const canvas = document.querySelector("#kirby");
+  const canvas = document.querySelector('#kirby');
   if (!canvas.getContext) return;
 
-  const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "#f0f0f0";
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#f0f0f0';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   const center = { x: 100, y: 100 };
   ctx.lineCap = 'round';
@@ -16,7 +16,7 @@ const drawKirby = () => {
   ctx.lineTo(center.x + 60, center.y + 78 + 8);
   ctx.bezierCurveTo(center.x + 80, center.y + 78 + 4, center.x + 80, center.y + 65, center.x + 50, center.y + 50);
   ctx.lineTo(center.x - 8, center.y + 78);
-  ctx.fillStyle = "#D40909";
+  ctx.fillStyle = '#D40909';
   ctx.fill();
   ctx.stroke();
 
@@ -24,7 +24,7 @@ const drawKirby = () => {
   ctx.beginPath();
   ctx.moveTo(center.x - 85, center.y + 25);
   ctx.bezierCurveTo(center.x - 105, center.y + 20, center.x - 95, center.y - 35, center.x - 62, center.y - 50);
-  ctx.fillStyle = "#F593AB";
+  ctx.fillStyle = '#F593AB';
   ctx.fill();
   ctx.stroke();
 
@@ -33,12 +33,12 @@ const drawKirby = () => {
   ctx.moveTo(center.x + 80, center.y);
   ctx.bezierCurveTo(center.x + 95, center.y - 40, center.x + 80, center.y - 90, center.x + 35, center.y - 70);
   ctx.closePath();
-  ctx.fillStyle = "#F593AB";
+  ctx.fillStyle = '#F593AB';
   ctx.fill();
   ctx.stroke();
 
   // Head
-  ctx.fillStyle = "#F593AB";
+  ctx.fillStyle = '#F593AB';
   ctx.beginPath();
   ctx.arc(center.x, center.y, 80, 0, Math.PI * 2);
   ctx.fill();
@@ -52,8 +52,8 @@ const drawKirby = () => {
   ctx.stroke();
 
   // Blush L
-  ctx.filter = "blur(2px)";
-  ctx.fillStyle = "#F9495C";
+  ctx.filter = 'blur(2px)';
+  ctx.fillStyle = '#F9495C';
   ctx.beginPath();
   ctx.ellipse(center.x - 32, center.y, 10, 4, 0, 0, 2 * Math.PI);
   ctx.fill();
@@ -63,9 +63,9 @@ const drawKirby = () => {
   ctx.fill();
 
   // Mouth
-  ctx.filter = "none";
+  ctx.filter = 'none';
   ctx.lineWidth = 1.4;
-  ctx.fillStyle = "black";
+  ctx.fillStyle = 'black';
   ctx.beginPath();
   ctx.ellipse(center.x, center.y + 10, 8, 6, 0, 0, Math.PI);
   ctx.stroke();
@@ -79,15 +79,15 @@ const drawKirby = () => {
     center.y - 25,
     24
   );
-  eyeGradientL.addColorStop(0, "black");
-  eyeGradientL.addColorStop(0.6, "#05448C");
+  eyeGradientL.addColorStop(0, 'black');
+  eyeGradientL.addColorStop(0.6, '#05448C');
   ctx.fillStyle = eyeGradientL;
   ctx.beginPath();
   ctx.ellipse(center.x - 14, center.y - 25, 7.2, 21, 0, 0, 2 * Math.PI);
   ctx.fill();
   ctx.stroke();
 
-  ctx.fillStyle = "white";
+  ctx.fillStyle = 'white';
   ctx.beginPath();
   ctx.ellipse(center.x - 14, center.y - 33, 4.7, 10, 0, 0, 2 * Math.PI);
   ctx.fill();
@@ -101,15 +101,15 @@ const drawKirby = () => {
     center.y - 25,
     24
   );
-  eyeGradientR.addColorStop(0, "black");
-  eyeGradientR.addColorStop(0.6, "#05448C");
+  eyeGradientR.addColorStop(0, 'black');
+  eyeGradientR.addColorStop(0.6, '#05448C');
   ctx.fillStyle = eyeGradientR;
   ctx.beginPath();
   ctx.ellipse(center.x + 14, center.y - 25, 7.2, 21, 0, 0, 2 * Math.PI);
   ctx.fill();
   ctx.stroke();
 
-  ctx.fillStyle = "white";
+  ctx.fillStyle = 'white';
   ctx.beginPath();
   ctx.ellipse(center.x + 14, center.y - 33, 4.7, 10, 0, 0, 2 * Math.PI);
   ctx.fill();
@@ -122,7 +122,7 @@ const drawKirby = () => {
   ctx.rotate(Math.PI / 3.5);
   ctx.translate(-center.x + 50, -center.y - 50);
   ctx.ellipse(center.x - 50, center.y + 50, 50, 30, 0, 0, 2 * Math.PI);
-  ctx.fillStyle = "#D40909";
+  ctx.fillStyle = '#D40909';
   ctx.fill();
   ctx.stroke();
   ctx.restore();
